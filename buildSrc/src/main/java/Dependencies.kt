aabdb75f1cf1c66bda9d67a0803aec93f02b555e
Happy Singh <happysingh23828@gmail.com>
2020-03-31 22:36:07 +0530

    Dependencies added

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,119 @@
+package dependencies
+
+object Dependencies {
+
+    private const val path = "../commonFiles/gradleScript/"
+    const val dependency = "./gradleScript/dependencies.gradle"
+    const val common = "${path}common.gradle"
+
+
+    object Module {
+        const val data = ":data"
+        const val cache = ":cache"
+        const val remote = ":remote"
+        const val domain = ":domain"
+    }
+
+    object Facebook {
+        const val stetho = "com.facebook.stetho:stetho:${Version.stetho}"
+        const val stethoNetwork = "com.facebook.stetho:stetho-okhttp3:${Version.stetho}"
+    }
+
+    object Lifecycle {
+        const val extension = "android.arch.lifecycle:extensions:${Version.lifecycle}"
+        const val annotation_compliler = "android.arch.lifecycle:compiler:${Version.lifecycle}"
+        const val testing_core_testing = "android.arch.core:core-testing:${Version.lifecycle}"
+        // ViewModel and LiveData
+        const val lifeCycleExtension =
+            "androidx.lifecycle:lifecycle-extensions:${Version.lifecycleVersion}"
+    }
+
+    object Glide {
+        const val glide = "com.github.bumptech.glide:glide:${Version.glide}"
+        const val annotationProcessor = "androidx.annotation:annotation:1.0.0"
+        const val annotationCompiler = "com.github.bumptech.glide:compiler:${Version.glide}"
+    }
+
+    object Gson {
+        const val gson = "com.google.code.gson:gson:${Version.gson}"
+    }
+
+    object Kotlin {
+        const val kotlin_stdlib_jdk7 = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Version.kotlin}"
+    }
+
+    object OkHttp3 {
+        const val loggingInterceptor =
+            "com.squareup.okhttp3:logging-interceptor:${Version.retrofit_log}"
+        const val okHttp3 = "com.squareup.okhttp3:okhttp:3.12.1"
+    }
+
+    object Retrofit2 {
+        const val adapterRxjava2 = "com.squareup.retrofit2:adapter-rxjava2:${Version.retrofit}"
+        const val converterGson = "com.squareup.retrofit2:converter-gson:${Version.retrofit}"
+        const val retrofit = "com.squareup.retrofit2:retrofit:${Version.retrofit}"
+    }
+
+    object AndroidX {
+        const val fragment = "androidx.fragment:fragment:${Version.androidx}"
+        const val annotation = "androidx.annotation:annotation:${Version.androidx}"
+        const val core = "androidx.core:core:${Version.androidx}"
+        const val constraintlayout =
+            "androidx.constraintlayout:constraintlayout:${Version.androidx_112}"
+        const val materialDesign = "com.google.android.material:material:${Version.materialDesign}"
+        const val support_recyclerview_v7 =
+            "androidx.recyclerview:recyclerview:${Version.recyclerView}"
+        const val appcompat = "androidx.appcompat:appcompat:${Version.androidx_100beta01}"
+        const val vectordrawable =
+            "androidx.vectordrawable:vectordrawable:${Version.androidx_100beta01}"
+    }
+
+    object Room {
+        const val runtime = "androidx.room:room-runtime:${Version.room}"
+        const val rxjava2 = "androidx.room:room-rxjava2:${Version.room}"
+        const val annotationProcessor = "androidx.room:room-compiler:${Version.room}"
+    }
+
+    object RxJava {
+        const val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Version.rxAndroid}"
+        const val rxjava2 = "io.reactivex.rxjava2:rxjava:${Version.rx}"
+        const val rxrelay2 = "com.jakewharton.rxrelay2:rxrelay:${Version.rxRelay}"
+        const val rxBinding = "com.jakewharton.rxbinding3:rxbinding:${Version.rxBinding}"
+    }
+
+    object Dagger {
+        const val dagger2 = "com.google.dagger:dagger:${Version.dagger2}"
+        const val daggerAndroid = "com.google.dagger:dagger-android:${Version.dagger2}"
+        const val daggerAndroidSupport =
+            "com.google.dagger:dagger-android-support:${Version.dagger2}"
+        const val processor = "com.google.dagger:dagger-android-processor:${Version.dagger2}"
+        const val compiler = "com.google.dagger:dagger-compiler:${Version.dagger2}"
+    }
+
+    object Test {
+        const val test_junit = "junit:junit:${Version.junit}"
+        const val android_test_espresso_core =
+            "com.android.support.test.espresso:espresso-core:${Version.espresso}"
+        const val android_test_room = "android.arch.persistence.room:testing:${Version.room}"
+        const val android_test_rules = "com.android.support.test:rules:${Version.rules}"
+        const val android_test_runner = "com.android.support.test:runner:${Version.runner}"
+    }
+
+
+    object Support {
+        const val supportV4 = "com.android.support:support-v4:${Version.supportLib}"
+    }
+
+    const val javax = "javax.inject:javax.inject:${Version.javaxInject}"
+    const val javaxjsr250 = "javax.annotation:jsr250-api:${Version.javaxAnnotation}"
+    const val parceler = "org.parceler:parceler-api:${Version.parcelerVersion}"
+    const val parcelerProcessor = "org.parceler:parceler-api:${Version.parcelerVersion}"
+
+    object ClassPaths {
+        const val gradleClasspath = "com.android.tools.build:gradle:${Version.gradleVersion}"
+        const val kotlinGradlePluginClasspath =
+            "org.jetbrains.kotlin:kotlin-gradle-plugin:${Version.kotlinVersion}"
+        const val dokkaPluginClasspath =
+            "org.jetbrains.dokka:dokka-gradle-plugin:${Version.dokkaVeresion}"
+    }
+}
---
+++ buildSrc/src/main/java/Version.kt
@@ -0,0 +1,44 @@
+package dependencies
+
+object Version {
+
+    // android configuration
+    const val buildTools = "27.0.3"
+    const val compileSdk = 28
+    const val minSdk = 21
+    const val targetSdk = 28
+    const val versionCode = 1
+    const val versionName = "1.0"
+
+    //Libraries
+    const val supportLib = "28.0.0"
+    const val recyclerView = "1.0.0"
+    const val androidx = "1.0.0"
+    const val androidx_112 = "1.1.3"
+    const val androidx_100beta01 = "1.0.0-beta01"
+    const val materialDesign = "1.0.0-rc01"
+    const val dagger2 = "2.21"
+    const val lifecycleVersion = "2.0.0"
+    const val javaxInject = "1"
+    const val javaxAnnotation = "1.0"
+    const val parcelerVersion = "1.1.12"
+    const val gradleVersion = "3.5.0"
+    const val kotlinVersion = "1.3.50"
+    const val dokkaVeresion = "0.9.18"
+    const val espresso = "3.0.2"
+    const val glide = "4.9.0"
+    const val gson = "2.8.5"
+    const val junit = "4.12"
+    const val kotlin = "1.3.31"
+    const val lifecycle = "1.1.1"
+    const val retrofit = "2.5.0"
+    const val retrofit_log = "3.10.0"
+    const val room = "2.0.0-rc01"
+    const val rules = "0.5"
+    const val runner = "1.0.2"
+    const val rx = "2.1.0"
+    const val rxAndroid = "2.0.1"
+    const val rxRelay = "2.0.0"
+    const val rxBinding = "3.0.0"
+    const val stetho = "1.5.0"
+}
