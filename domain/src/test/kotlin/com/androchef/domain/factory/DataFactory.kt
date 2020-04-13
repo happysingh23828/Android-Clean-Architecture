9121151cf2d4a5bcc85e103b2a7856c1d1fdf0e2
Happy Singh <happysingh23828@gmail.com>
2020-04-13 20:54:34 +0530

    Unit test case added for GetMovieListUseCase.

---
+++ domain/src/test/kotlin/com/androchef/domain/factory/DataFactory.kt
@@ -0,0 +1,27 @@
+package com.androchef.domain.factory
+
+import java.util.*
+import kotlin.random.Random
+
+object DataFactory {
+
+    fun getRandomString(): String {
+        return UUID.randomUUID().toString().substring(0, 15)
+    }
+
+    fun getRandomInt(): Int {
+        return Random.nextInt()
+    }
+
+    fun getRandomLong(): Long {
+        return Random.nextLong()
+    }
+
+    fun getRandomDouble(): Double {
+        return Random.nextDouble()
+    }
+
+    fun getRandomBoolean() : Boolean {
+        return Random.nextBoolean()
+    }
+}
---
+++ domain/src/test/kotlin/com/androchef/domain/factory/movies/MoviesFactory.kt
@@ -0,0 +1,40 @@
+package com.androchef.domain.factory.movies
+
+import com.androchef.domain.factory.DataFactory
+import com.androchef.domain.models.movies.Movie
+
+object MoviesFactory {
+
+    fun generateDummyMovieList(size: Int) : List<Movie> {
+        val movieList = mutableListOf<Movie>()
+        repeat(size) {
+            movieList.add(generateMovie())
+        }
+        return movieList
+    }
+
+    fun generateMovie(): Movie {
+        return Movie(
+            adult = DataFactory.getRandomBoolean(),
+            isBookMarked = DataFactory.getRandomBoolean(),
+            id = DataFactory.getRandomLong(),
+            name = DataFactory.getRandomString(),
+            title = DataFactory.getRandomString(),
+            vote_average = DataFactory.getRandomDouble(),
+            profile_path = DataFactory.getRandomString(),
+            poster_path = DataFactory.getRandomString(),
+            popularity = DataFactory.getRandomDouble(),
+            backdrop_path = DataFactory.getRandomString(),
+            first_air_date = DataFactory.getRandomString(),
+            genre_ids = listOf(DataFactory.getRandomInt(), DataFactory.getRandomInt()),
+            known_for_department = DataFactory.getRandomString(),
+            original_language = DataFactory.getRandomString(),
+            original_title = DataFactory.getRandomString(),
+            overview = DataFactory.getRandomString(),
+            release_date = DataFactory.getRandomString(),
+            video = DataFactory.getRandomBoolean(),
+            vote_count = DataFactory.getRandomInt()
+        )
+    }
+
+}
