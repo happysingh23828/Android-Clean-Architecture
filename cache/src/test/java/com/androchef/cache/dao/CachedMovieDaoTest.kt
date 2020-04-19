93a14839788de558f5ab3eebab35437926b7bc86
Happy Singh <happysingh23828@gmail.com>
2020-04-19 17:46:55 +0530

    Removed warning for unit test cases.

--- cache/src/test/java/com/androchef/cache/dao/CachedMovieDaoTest.kt
+++ cache/src/test/java/com/androchef/cache/dao/CachedMovieDaoTest.kt
@@ -9,7 +9,9 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
 
+@Config(manifest= Config.NONE)
 @RunWith(RobolectricTestRunner::class)
 class CachedMovieDaoTest {
 
