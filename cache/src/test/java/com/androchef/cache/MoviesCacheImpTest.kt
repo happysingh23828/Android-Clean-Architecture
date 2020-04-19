438a5104bceca4fe245c7ed7fe56152e40e4811b
Happy Singh <happysingh23828@gmail.com>
2020-04-19 17:46:55 +0530

    Removed warning for unit test cases.

--- cache/src/test/java/com/androchef/cache/MoviesCacheImpTest.kt
+++ cache/src/test/java/com/androchef/cache/MoviesCacheImpTest.kt
@@ -10,7 +10,9 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
 
+@Config(manifest= Config.NONE)
 @RunWith(RobolectricTestRunner::class)
 class MoviesCacheImpTest {
 
