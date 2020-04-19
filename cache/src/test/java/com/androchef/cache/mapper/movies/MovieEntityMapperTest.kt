74c6f6b483c7dcf46501021b77388977c8fe8635
Happy Singh <happysingh23828@gmail.com>
2020-04-19 14:47:09 +0530

    Cache module test cases added.

---
+++ cache/src/test/java/com/androchef/cache/mapper/movies/MovieEntityMapperTest.kt
@@ -0,0 +1,56 @@
+package com.androchef.cache.mapper.movies
+
+import com.androchef.cache.models.CachedMovie
+import com.androchef.cache.test.factory.cached_movie.CachedMovieFactory
+import com.androchef.data.models.MovieEntity
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.runners.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class MovieEntityMapperTest {
+
+    lateinit var movieEntityMapper: MovieEntityMapper
+
+
+    @Before
+    fun setUp() {
+        movieEntityMapper = MovieEntityMapper()
+    }
+
+    @Test
+    fun mapFromCached() {
+        //Arrange
+        val cacheMovie = CachedMovieFactory.generateCachedMovie()
+
+        //Act
+        val movieEntity = movieEntityMapper.mapFromCached(cacheMovie)
+
+        //Assert
+        assertMovieDataEquality(cacheMovie,movieEntity)
+    }
+
+    @Test
+    fun mapToCache() {
+        //Arrange
+        val movieEntity = CachedMovieFactory.generateMovieEntity()
+
+        //Act
+        val cacheMovie = movieEntityMapper.mapToCached(movieEntity)
+
+        //Assert
+        assertMovieDataEquality(cacheMovie,movieEntity)
+    }
+
+    private fun assertMovieDataEquality(cachedMovie: CachedMovie, movieEntity: MovieEntity) {
+        Assert.assertEquals(movieEntity.id, cachedMovie.movieId)
+        Assert.assertEquals(movieEntity.movieName, cachedMovie.name)
+        Assert.assertEquals(movieEntity.movieTitle, cachedMovie.title)
+        Assert.assertEquals(movieEntity.posterPath, cachedMovie.posterPath)
+        Assert.assertEquals(movieEntity.voteAverage, cachedMovie.voteAverage)
+        Assert.assertEquals(movieEntity.profilePath, cachedMovie.profilePath)
+        Assert.assertEquals(movieEntity.isBookMarked, cachedMovie.isBookMarked)
+    }
+}
---
+++ cache/src/test/java/com/androchef/cache/test/factory/cached_movie/CachedMovieFactory.kt
@@ -0,0 +1,59 @@
+package com.androchef.cache.test.factory.cached_movie
+
+import com.androchef.cache.models.CachedMovie
+import com.androchef.cache.test.factory.DataFactory
+import com.androchef.data.models.MovieEntity
+
+object CachedMovieFactory {
+
+    fun generateListOfCachedMovie(size: Int): MutableList<CachedMovie> {
+        val listOfCachedMovie = mutableListOf<CachedMovie>()
+        repeat(size) {
+            listOfCachedMovie.add(generateCachedMovie())
+        }
+        return listOfCachedMovie
+    }
+
+    fun generateListOfMovieEntities(size: Int): MutableList<MovieEntity> {
+        val listOfMovieEntities = mutableListOf<MovieEntity>()
+        repeat(size) {
+            listOfMovieEntities.add(generateMovieEntity())
+        }
+        return listOfMovieEntities
+    }
+
+    fun generateListOfMovieEntitiesWithNotBookmark(size: Int): MutableList<MovieEntity> {
+        val listOfMovieEntities = mutableListOf<MovieEntity>()
+        repeat(size) {
+            val movieEntity = generateMovieEntity()
+            movieEntity.isBookMarked = false
+            listOfMovieEntities.add(movieEntity)
+        }
+        return listOfMovieEntities
+    }
+
+
+    fun generateCachedMovie(): CachedMovie {
+        return CachedMovie(
+            movieId = DataFactory.getRandomLong(),
+            posterPath = DataFactory.getRandomString(),
+            profilePath = DataFactory.getRandomString(),
+            voteAverage = DataFactory.getRandomDouble(),
+            isBookMarked = DataFactory.getRandomBoolean(),
+            title = DataFactory.getRandomString(),
+            name = DataFactory.getRandomString()
+        )
+    }
+
+    fun generateMovieEntity(): MovieEntity {
+        return MovieEntity(
+            id = DataFactory.getRandomLong(),
+            posterPath = DataFactory.getRandomString(),
+            profilePath = DataFactory.getRandomString(),
+            voteAverage = DataFactory.getRandomDouble(),
+            isBookMarked = DataFactory.getRandomBoolean(),
+            movieTitle = DataFactory.getRandomString(),
+            movieName = DataFactory.getRandomString()
+        )
+    }
+}
